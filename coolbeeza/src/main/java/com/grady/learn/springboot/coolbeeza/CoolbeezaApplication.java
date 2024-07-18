1089f151e61494a047e945f936cd92a5da4ea96c
grady.liu <grady0902@gmail.com>
2024-07-19 00:23:39 +0800

    chore: Add initial project files and configurations

---
+++ coolbeeza/src/main/java/com/grady/learn/springboot/coolbeeza/CoolbeezaApplication.java
@@ -0,0 +1,13 @@
+package com.grady.learn.springboot.coolbeeza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolbeezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolbeezaApplication.class, args);
+	}
+
+}
---
+++ coolbeeza/src/test/java/com/grady/learn/springboot/coolbeeza/CoolbeezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grady.learn.springboot.coolbeeza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolbeezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
