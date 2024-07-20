6304d98076d0360bd051b85f0de95c0a84c02aa5
grady.liu <grady0902@gmail.com>
2024-07-20 16:12:52 +0800

    feat: Add CoreRestController with home endpoint

---
+++ coolbeeza/src/main/java/com/grady/learn/springboot/coolbeeza/rest/CoreRestController.java
@@ -0,0 +1,13 @@
+package com.grady.learn.springboot.coolbeeza.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoreRestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+}
