d74056ed03d1c2182edfc60ae3d5b4affb659b23
grady.liu <grady0902@gmail.com>
2024-07-25 14:56:47 +0800

    chore: Add dependencies for Spring Data JPA and MySQL Connector

--- coolbeeza/src/main/java/com/grady/learn/springboot/coolbeeza/CoolbeezaApplication.java
+++ coolbeeza/src/main/java/com/grady/learn/springboot/coolbeeza/CoolbeezaApplication.java
@@ -1,7 +1,9 @@
 package com.grady.learn.springboot.coolbeeza;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoolbeezaApplication {
@@ -10,4 +12,13 @@ public class CoolbeezaApplication {
 		SpringApplication.run(CoolbeezaApplication.class, args);
 	}
 
+	// from the spring boot framework
+	// Executed after the Spring Bean have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		// Java lambda expression
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
